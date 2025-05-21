5741680c1354847ceaf78469c55dcbc0fdf5e57f
LelandMitchell2 <leland.a.mitchell2@gmail.com>
2025-05-20 21:56:02 -0400

    Delete Server directory
    Removing duplicate server folder to hopefully resolve some issues will re upload all files to a single server folder

--- Server/src/models/index.ts
+++
@@ -1,7 +0,0 @@
-import User from './User.js';
-import Project from './Projects.js';
-import Task from './Task.js';
-import BudgetItem from './BudgetItem.js';
-import Material from './Material.js';
-
-export { User, Project, Task, BudgetItem, Material };
