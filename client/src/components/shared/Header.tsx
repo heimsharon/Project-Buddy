0abdd176b6b5b48a817007b295515dc40afa0372
TonesTheDude <alangdon1@gmail.com>
2025-05-22 22:11:55 -0400

    continue header

--- client/src/components/shared/Header.tsx
+++ client/src/components/shared/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -6,6 +7,10 @@ const Header = () => {
       <div className="flex items-center space-x-4">
         <img src="/src/assets/image.png" alt="Logo" className="h-8 w-auto" />
         <nav className="flex space-x-6 text-gray-700 text-sm font-medium">
+        <Link to = "/login"><button>Login</button></Link>
+        <Link to = "/signup"><button>Sign Up</button></Link>
+          <a href="/" className="hover:text-blue-600">Home Page</a>
+          <a href="/profiles/:username" className="hover:text-blue-600">Profile</a>
           <a href="#" className="hover:text-blue-600">My Projects</a>
           <a href="#" className="hover:text-blue-600">Estimator</a>
           <a href="#" className="hover:text-blue-600">Budget</a>
