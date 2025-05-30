914ea5dc206db04725b5ce5eeebec4ac3eae4a8c
Shane Anderson <shane.d.anderson@outlook.com>
2025-05-29 21:27:03 -0400

    corrected lingering conflicts

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -88,14 +88,84 @@ export const QUERY_SINGLE_TASK = gql`
     notes
     projectId {
       _id
-      username
-      email
-      thoughts {
-        _id
-        thoughtText
-        thoughtAuthor
-        createdAt
-      }
     }
+    title
   }
+}
+`;
+
+export const QUERY_ALL_BUDGET_ITEMS = gql`
+  query GetAllBudgetItems {
+  getAllBudgetItems {
+    _id
+    cost
+    name
+    notes
+    quantity
+  }
+}
+`;
+
+export const QUERY_SINGLE_BUDGET_ITEM = gql`
+  query GetBudgetItemById($getBudgetItemByIdId: ID!) {
+  getBudgetItemById(id: $getBudgetItemByIdId) {
+    _id
+    cost
+    name
+    notes
+    quantity
+  }
+}
+`;
+
+export const QUERY_ALL_MATERIALS = gql`
+  query GetAllMaterials {
+  getAllMaterials {
+    _id
+    category
+    lastUpdated
+    name
+    priceUSD
+    quantity
+    unit
+    unitCoverage {
+      height_ft
+      length_ft
+      length_in
+      sqft
+      thickness_in
+      weight_lb
+      weight_ton
+      width_ft
+      width_in
+    }
+    vendor
+  }
+}
+`;
+
+export const QUERY_SINGLE_MATERIAL = gql`
+  query GetMaterialById($getMaterialByIdId: ID!) {
+  getMaterialById(id: $getMaterialByIdId) {
+    _id
+    category
+    lastUpdated
+    name
+    priceUSD
+    quantity
+    unit
+    unitCoverage {
+      length_ft
+      width_ft
+      height_ft
+      width_in
+      length_in
+      thickness_in
+      weight_lb
+      weight_ton
+      sqft
+    }
+    vendor
+  }
+}
 `;
